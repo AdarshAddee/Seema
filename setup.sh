5c0d5677c1524eebe33c4c59b84ddc728da9a57e
Adarsh Addee <59617563+adarshaddee@users.noreply.github.com>
2020-08-27 11:19:20 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,31 @@
+echo -e "\n"
+echo -ne "Installation Begins ###"
+for i in 1 2 3 4 5
+do
+    echo -ne "#"
+    sleep 0.5
+done
+echo -e "\n"
+
+echo "Checking for system update"
+apt update && apt upgrade -y
+
+echo -e "\n"
+
+echo "Checking for python and it's updates"
+sleep 0.5
+apt-get install python
+sleep 0.5
+python3 -m pip install --upgrade pip
+
+echo -e "\n"
+sleep 0.5
+echo "Checking for Required Python Pakages"
+sleep 1
+pip install -r requirements.txt
+
+sleep 0.5
+
+echo "Installation Completed"
+echo "Now, You can use your tool"
+
